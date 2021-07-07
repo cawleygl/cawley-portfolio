3513b5edaa53c5dbdeb5113c6516dcd78f77a05f
cawleygl <grantlcawley@gmail.com>
2021-07-07 03:42:42 -0400

    Style changes

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -6,10 +6,10 @@ import "./style.css"
 const card = (props) => {
     return (
         <Container fluid>
-            <Card className="card" style={{ width: '18rem' }}>
+            <Card style={{ width: '20rem' }}>
                 <Card.Img variant="top" src={props.asset} />
                 <Card.Body>
-                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Header>{props.title}</Card.Header>
                     <Card.Text>{props.text}</Card.Text>
                     <ButtonGroup>
                         <Button size="sm" href={props.appLink} variant="danger">Application</Button>
