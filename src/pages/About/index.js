928b9afa0e6aaf38e52da71d4b44d80f0b7f0f60
Grant Cawley <grantlcawley@gmail.com>
2022-05-02 16:24:37 -0400

    update 5/2/22

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -25,7 +25,7 @@ const AboutPage = () => {
                         </Row> */}
         </Col>
         <Col className="about">
-          <p>Grant Cawley is a full-stack software developer currently located in Raleigh, North Carolina, with a certificate in Full Stack Web Development from Washington University Coding Boot Camp. He is currently working remotely for Hopdrive in Richmond, Virginia as a Junior Frontend Developer: a web development role utilizing React, Javascript, GraphQL, MUI, Node.js, REST APIs, and other technologies. Grant graduated from Saint Louis University in the Spring of 2018 with a Bachelor of Science degree in Psychology, and Bachelor of Arts degree in Spanish Language and Literature. He has completed a number of coding projects through the Washington University Coding Boot Camp program, included in this portfolio. </p>
+          <p>My name is Grant Cawley, and I am a Full-Stack Software Developer currently located in Charlotte, North Carolina, with a certificate in Full-Stack Web Development from Washington University Coding Boot Camp. I am currently employed by Capgemini in Charlotte as a Software Engineer in their Next-Generation Talent program. I previously worked for Hopdrive in Richmond Virginia as a Junior Frontend Developer: a web development role utilizing React, Javascript, GraphQL, MUI, Node.js, REST APIs, and other technologies. I graduated from Saint Louis University in the Spring of 2018 with a Bachelor of Science degree in Psychology and Spanish Language and Literature. I have completed a number of coding projects through the Washington University Coding Boot Camp program, included in this portfolio.</p>
         </Col>
       </Row>
     </Container>
